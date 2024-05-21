7abbfb412cedfcd3fcde81537824e401a72cd6d4
akashmhet0001 <169160055+akashmhet0001@users.noreply.github.com>
2024-05-21 18:26:33 -0400

    JS file added

---
+++ index.js
@@ -0,0 +1,30 @@
+//Task 1
+let a = 3;
+let b = 5;
+let c;
+
+
+console.log("a + b = " + (a + b) + "\n" +
+      "a - b = " + (a - b) + "\n" +
+      "a * b = " + (a * b) + "\n" +
+      "a / b = " + (a / b) + "\n" +
+      "a % b = " + (a % b) + "\n" +
+      "a += b = " + (a += b) + "\n" +
+      "a -= b = " + (a -= b) + "\n" +
+      "a = b = " + (a= b) + "\n" +
+      "a /= b = " + (a /= b) + "\n" +
+      "a %= b = " + (a %= b) + "\n" +
+      "a == b = " + (a == b) + "\n" +
+      "a != b = " + (a != b) + "\n" +
+      "a > b = " + (a > b) + "\n" +
+      "a < b = " + (a < b) + "\n" +
+      "!a && !c = " + (!a && !c) + "\n" +
+      "!a || !c = " + (!a || !c));
+
+
+// Task 2
+let first_name = "Akash";
+let last_name = "Mhetre";
+let email = "mhet0001@algonquinlive.com";
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+console.log(output);
